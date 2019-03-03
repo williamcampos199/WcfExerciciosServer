e800932f730efc6a62a27519d725c306bee5722a
William Campos <william_apr16@hotmail.com>
2019-03-03 13:31:35 -0300

    Primeiro Commit

---
+++ WCFExerciciosService/WCFExerciciosService/App_Code/Dal/Conexao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de Conexao
+/// </summary>
+public class Conexao
+{
+
+public static string getConexao()
+    {
+        return @"Data Source=.\SQLEXPRESS;Initial Catalog=Academia;Integrated Security=True";
+    }
+}
---
+++ WCFExerciciosService/WCFExerciciosService/App_Code/Model/Exercicio.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+/// <summary>
+/// Descrição resumida de Exercicio
+/// </summary>
+/// 
+
+[DataContract]
+public class Exercicio
+{
+    [DataMember]
+    public int idExercicio { get; set; }
+    [DataMember]
+    public string tipo { get; set; }
+    [DataMember]
+    public string nome { get; set; }
+    [DataMember]
+    public string repeticoes { get; set; }
+    [DataMember]
+    public string observacao { get; set; }
+
+    public Exercicio()
+    {
+        //
+        // TODO: Adicionar lógica do construtor aqui
+        //
+    }
+
+
+
+
+}
